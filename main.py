7489689aa6371a09fed440fd4a09c4b2a0bd5177
merima98 <merimaceranic98@gmail.com>
2025-12-30 11:28:14 +0100

    Creating Exercise to practice Python

--- main.py
+++ main.py
@@ -1,10 +1,37 @@
+# Arcade Day Pass Tracker - Challlenge Steps
 
-failed_subjects='2'
-name='John'
+# 1) Create variables to store:
+#  - customer name
+#  - number of passes
+#  - tokens per pass
+#  - price per pass
+#  - tokens required per game
 
-print('Dear Mrs Badger')
-print ('Your son '+ name +  ' is failing '+ failed_subjects +  ' subjects')
-print( name +  ' will need to redo '+ failed_subjects +  ' courses')
+customer_name = "Alice"
+number_of_passes = 3
+tokens_per_pass = 50
+price_per_pass = 20.0
+tokens_per_game = 15
 
-name='Eric'
-print( name +  ' is doing well in geography')
+
+
+# 2) Calculate:
+#  - total tokens
+total_tokens = number_of_passes * tokens_per_pass
+#  - total cost
+total_cost = number_of_passes * price_per_pass
+#  - games available (use 'floor division' to get a whole number)
+games_available = total_tokens // tokens_per_game
+
+# 3) Print a summary with :
+#  - customer name
+#  - passes bought
+#  - total tokens
+#  - total cost
+#  - games available
+
+print('Customer Name:', customer_name)
+print('Passes Bought:', number_of_passes)
+print('Total Tokens:', total_tokens)
+print('Total Cost:', total_cost)
+print('Games Available:', games_available)
